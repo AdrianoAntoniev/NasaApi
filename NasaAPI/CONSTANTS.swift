9eb251f16fa5cdf1c16ed786265d82ace833cf9d
AdrianoAntoniev <adriano.antoniev@outlook.com>
2022-11-24 00:31:26 -0300

    :sparkles: Initial commit

---
+++ NasaAPI/CONSTANTS.swift
@@ -0,0 +1,12 @@
+//
+//  CONSTANTS.swift
+//  NasaAPI
+//
+//  Created by Adriano Rodrigues Vieira on 24/11/22.
+//
+
+import Foundation
+
+public enum Constants {
+    static let API_KEY = "Uk1F4ilKjeA3Z5rxDviKJR1qDba7oeKbY4SZAgmL"
+}
---
+++ NasaAPI/NasaAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPIApp.swift
+//  NasaAPI
+//
+//  Created by Adriano Rodrigues Vieira on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NasaAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
